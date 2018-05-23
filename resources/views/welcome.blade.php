05b758540d807619dcaea1a1a5b9885c7f9a107a
Mario Sanchez <mario.sanchez@ontwice.com>
2018-05-23 18:35:01 -0500

    Cambio de nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mariossan
                 </div>
 
                 <div class="links">
